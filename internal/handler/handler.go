6350ef98c6f64695217c02197f38016b3fc689d2
Oleg Kishinsky <kishinsky_oleg@softforsports.com>
2025-09-01 02:21:06 +0300

    add hadler chi

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,27 @@
 package handler
 
-import "github.com/go-chi/chi/v5"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type hadlerFunc func(http.ResponseWriter, *http.Request) error
 
 func RegisterRouters(r *chi.Mux) {
 	home := homeHandler{}
-	r.Get("/", home.handleIndex)
+	r.Get("/", handler(home.handleIndex))
+}
+
+func handler(h hadlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := h(w, r); err != nil {
+			handleError(w, r, err)
+		}
+	}
+}
+
+func handleError(w http.ResponseWriter, r *http.Request, err error) {
+	slog.Error("Error during request", slog.String("error", err.Error()))
 }
--- internal/handler/home.go
+++ internal/handler/home.go
@@ -8,6 +8,10 @@ import (
 type homeHandler struct {
 }
 
-func (h homeHandler) handleIndex(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World")
+func (h homeHandler) handleIndex(w http.ResponseWriter, r *http.Request) error {
+	_, err := io.WriteString(w, "Hello World")
+	if err != nil {
+		return err
+	}
+	return nil
 }
